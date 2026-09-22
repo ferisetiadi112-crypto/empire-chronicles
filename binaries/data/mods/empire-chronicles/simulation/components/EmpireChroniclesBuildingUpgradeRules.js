bd35a424be74043ed798fb81ea3f43e3ed83f766
ferisetiadi112-crypto <ferisetiadi112@gmail.com>
2026-09-22 17:54:10 +0700

    feat: read upgrade thresholds from building catalog

--- binaries/data/mods/empire-chronicles/simulation/components/EmpireChroniclesBuildingUpgradeRules.js
+++ binaries/data/mods/empire-chronicles/simulation/components/EmpireChroniclesBuildingUpgradeRules.js
@@ -1,7 +1,8 @@
 // Empire Chronicles building upgrade rules.
 //
-// Separates automatic community growth from deliberate player upgrades.
-// Reserved development space is assumed to exist before an upgrade occurs.
+// Rules can be supplied directly by a building's upgrade-level catalog.
+// This keeps gameplay thresholds in the building definition instead of
+// duplicating them inside the component.
 
 function EmpireChroniclesBuildingUpgradeRules() {}
 
@@ -17,13 +18,33 @@ EmpireChroniclesBuildingUpgradeRules.prototype.Init = function()
 	this.minimumPopulation = +(this.template.MinimumPopulation || 0);
 	this.requiredWellbeing = +(this.template.RequiredWellbeing || 0);
 	this.requiredTechnology = +(this.template.RequiredTechnology || 1);
+	this.upgradeLevels = this.template.UpgradeLevels || [];
 };
 
-EmpireChroniclesBuildingUpgradeRules.prototype.CanUpgrade = function(population, wellbeing, technologyLevel)
+EmpireChroniclesBuildingUpgradeRules.prototype.SetUpgradeLevels = function(upgradeLevels)
 {
-	return population >= this.minimumPopulation &&
-		wellbeing >= this.requiredWellbeing &&
-		technologyLevel >= this.requiredTechnology;
+	this.upgradeLevels = upgradeLevels || [];
+};
+
+EmpireChroniclesBuildingUpgradeRules.prototype.GetNextLevelRule = function(currentLevel)
+{
+	for (var i = 0; i < this.upgradeLevels.length; ++i)
+		if (+this.upgradeLevels[i].level === currentLevel + 1)
+			return this.upgradeLevels[i];
+
+	return null;
+};
+
+EmpireChroniclesBuildingUpgradeRules.prototype.CanUpgrade = function(population, wellbeing, technologyLevel, currentLevel)
+{
+	var rule = this.GetNextLevelRule(+currentLevel || 0);
+
+	if (!rule)
+		return false;
+
+	return population >= +(rule.minimumPopulation || 0) &&
+		wellbeing >= +(rule.requiredWellbeing || 0) &&
+		technologyLevel >= +(rule.requiredTechnology || 1);
 };
 
 EmpireChroniclesBuildingUpgradeRules.prototype.IsAutomatic = function()
@@ -40,8 +61,6 @@ EmpireChroniclesBuildingUpgradeRules.prototype.GetState = function()
 {
 	return {
 		managementType: this.managementType,
-		minimumPopulation: this.minimumPopulation,
-		requiredWellbeing: this.requiredWellbeing,
-		requiredTechnology: this.requiredTechnology
+		upgradeLevels: this.upgradeLevels
 	};
 };
