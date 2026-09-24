e68adcdfcae990d119b15707460b5cb5f141a977
ferisetiadi112-crypto <ferisetiadi112@gmail.com>
2026-09-24 19:05:45 +0700

    Integrate settlement progression and cemetery gate

--- binaries/data/mods/empire-chronicles/simulation/components/EmpireChroniclesSettlement.js
+++ binaries/data/mods/empire-chronicles/simulation/components/EmpireChroniclesSettlement.js
@@ -1,7 +1,8 @@
-// Empire Chronicles settlement component.
+// Engine-facing settlement state for Empire Chronicles.
 //
 // Settlement territory is cell/tile based. A Government Office is the
 // settlement control center; it does not create a permanent circular boundary.
+// Population is part of the shared national pool, not a separate population pool.
 
 function EmpireChroniclesSettlement() {}
 
@@ -11,6 +12,12 @@ EmpireChroniclesSettlement.prototype.Schema =
 	"</element>" +
 	"<element name='CountryId'>" +
 		"<text/>" +
+	"</element>" +
+	"<element name='Population'>" +
+		"<integer/>" +
+	"</element>" +
+	"<element name='CemeteryLevel'>" +
+		"<integer/>" +
 	"</element>";
 
 EmpireChroniclesSettlement.prototype.Init = function()
@@ -18,12 +25,14 @@ EmpireChroniclesSettlement.prototype.Init = function()
 	this.settlementName = this.template.SettlementName || "New Settlement";
 	this.countryId = this.template.CountryId || "";
 	this.stage = "settlement";
-	this.population = 20;
+	this.population = this.template.Population || 20;
+	this.cemeteryLevel = this.template.CemeteryLevel || 0;
 	this.governmentOfficeId = null;
 	this.territoryCells = [];
 	this.buildings = [];
 	this.infrastructure = [];
 	this.active = false;
+	this.UpdateStage();
 };
 
 EmpireChroniclesSettlement.prototype.SetGovernmentOffice = function(entityId)
@@ -58,21 +67,65 @@ EmpireChroniclesSettlement.prototype.SetPopulation = function(population)
 		throw new Error("Settlement population cannot be negative");
 
 	this.population = population;
+	this.UpdateStage();
+};
+
+EmpireChroniclesSettlement.prototype.GetPopulation = function()
+{
+	return this.population;
+};
+
+EmpireChroniclesSettlement.prototype.SetCemeteryLevel = function(level)
+{
+	if (level < 0)
+		throw new Error("Cemetery level cannot be negative");
+
+	this.cemeteryLevel = level;
+	this.UpdateStage();
+};
+
+EmpireChroniclesSettlement.prototype.GetCemeteryLevel = function()
+{
+	return this.cemeteryLevel;
+};
+
+EmpireChroniclesSettlement.prototype.UpdateStage = function()
+{
 	this.stage = this.GetStage();
 };
 
 EmpireChroniclesSettlement.prototype.GetStage = function()
 {
-	if (this.population >= 160)
-		return "capital";
-	if (this.population >= 110)
-		return "province";
-	if (this.population >= 70)
-		return "city";
-	if (this.population >= 40)
-		return "town";
+	const progression = Engine.QueryInterface(
+		this.entity,
+		IID_EmpireChroniclesProgression
+	);
+	const technologyLevel = progression ?
+		progression.technologyLevel :
+		1;
 
-	return "settlement";
+	const stages = [
+		{ id: "settlement", population: 20 },
+		{ id: "town", population: 40 },
+		{ id: "city", population: 70, cemeteryLevel: 3 },
+		{ id: "province", population: 110 },
+		{ id: "capital", population: 160 }
+	];
+
+	let stage = "settlement";
+	for (const candidate of stages)
+	{
+		if (this.population < candidate.population)
+			break;
+		if (technologyLevel < 1)
+			break;
+		if (this.cemeteryLevel < (candidate.cemeteryLevel || 0))
+			break;
+
+		stage = candidate.id;
+	}
+
+	return stage;
 };
 
 EmpireChroniclesSettlement.prototype.GetState = function()
@@ -82,8 +135,15 @@ EmpireChroniclesSettlement.prototype.GetState = function()
 		countryId: this.countryId,
 		stage: this.stage,
 		population: this.population,
+		cemeteryLevel: this.cemeteryLevel,
 		governmentOfficeId: this.governmentOfficeId,
 		territoryCells: this.territoryCells.slice(),
 		active: this.active
 	};
 };
+
+Engine.RegisterComponentType(
+	IID_EmpireChroniclesSettlement,
+	"EmpireChroniclesSettlement",
+	EmpireChroniclesSettlement
+);
